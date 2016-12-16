0eaa7e257914532e0d25c8f7a8ad31809cb55c74
Michael Cheers <michaelcheers8@gmail.com>
2016-12-16 17:24:57 +0100

    Added hexagon lines.

---
+++ PolygonPermutations/app.js
@@ -0,0 +1,61 @@
+var linear = (function () {
+    function linear() {
+    }
+    linear.slope = function (x1, y1, x2, y2) {
+        if (x1 == x2)
+            console.log("error");
+        return (y1 - y2) / (x1 - x2);
+    };
+    linear.yInt = function (x1, y1, x2, y2) {
+        if (x1 === x2)
+            return y1 === 0 ? 0 : false;
+        if (y1 === y2)
+            return y1;
+        return y1 - linear.slope(x1, y1, x2, y2) * x1;
+    };
+    linear.getIntersection = function (x11, y11, x12, y12, x21, y21, x22, y22) {
+        var slope1, slope2, yint1, yint2, intx, inty;
+        if (x11 == x21 && y11 == y21)
+            return [x11, y11];
+        if (x12 == x22 && y12 == y22)
+            return [x12, y22];
+        slope1 = this.slope(x11, y11, x12, y12);
+        slope2 = this.slope(x21, y21, x22, y22);
+        if (slope1 === slope2)
+            return false;
+        yint1 = this.yInt(x11, y11, x12, y12);
+        yint2 = this.yInt(x21, y21, x22, y22);
+        if (yint1 === yint2)
+            return yint1 === false ? false : [0, yint1];
+        if (slope1 === undefined)
+            return [y21, slope2 * y21 + yint2];
+        if (slope2 === undefined)
+            return [y11, slope1 * y11 + yint1];
+        intx = (slope1 * x11 + yint1 - yint2) / slope2;
+        return [intx, slope1 * intx + yint1];
+    };
+    return linear;
+}());
+window.onload = function () {
+    var line = new Line({ x: 100, y: 100 });
+    line.penDown();
+    var hexagon = [];
+    for (var n = 0; n < 6; n++) {
+        hexagon.push(line.pos);
+        line.move(200);
+        line.turn(1 / 6);
+    }
+    var colors = ["#f00", "#00f", "#0f0", "#f0f", "#00f", "#ff0"];
+    var polygons = [hexagon];
+    var seperatingLines = [];
+    for (var n = 0; n < 6; n++) {
+        for (var n2 = 2; n2 < 4; n2++) {
+            line.changeColor(colors[n]);
+            line.moveTo(hexagon[n]);
+            line.lineTo(hexagon[(n2 + n) % 6]);
+        }
+    }
+    var path = line.SVGPaths;
+    path.forEach(function (v) { canvas.appendChild(v); });
+};
+//# sourceMappingURL=app.js.map
---
+++ PolygonPermutations/lib.ts
@@ -0,0 +1,93 @@
+﻿class Line
+{
+    pos: Vector2;
+    direction: number;
+    line: boolean;
+    pathD: string[];
+    colors: string[];
+
+    constructor(pos?: Vector2)
+    {
+        this.pos = pos || { x: 0, y: 0 };
+        this.direction = 1/4;
+        this.line = false;
+        this.pathD = [Line.vector2ToString("M", this.pos)];
+        this.colors = ["black"];
+    }
+
+    static vector2ToString(start: string, pos: Vector2)
+    {
+        return start + pos.x + " " + pos.y;
+    }
+
+    move(steps: number)
+    {
+        this.lineTo({ x: (Math.sin(this.directionInRadians) * steps) + this.pos.x, y: this.pos.y - (Math.cos(this.directionInRadians) * steps) });
+    }
+
+    changeColor(color: string)
+    {
+        this.colors.push(color);
+        this.pathD.push(Line.vector2ToString("M", this.pos));
+    }
+
+    turn(direction: number)
+    {
+        this.direction += direction;
+    }
+
+    lineTo(pos: Vector2)
+    {
+        this.pos = pos;
+        this.pathD[this.pathD.length - 1] += Line.vector2ToString(this.line ? " L" : " M", pos);
+    }
+
+    moveTo(pos: Vector2)
+    {
+        var oldLine = this.line;
+        this.penUp();
+        this.lineTo(pos);
+        this.line = oldLine;
+    }
+
+    penDown()
+    {
+        this.line = true;
+    }
+
+    penUp()
+    {
+        this.line = false;
+    }
+
+    get directionInRadians()
+    {
+        return this.direction * (Math.PI * 2);
+    }
+
+    set directionInRadians(radians: number)
+    {
+        this.direction = (radians * 2) / Math.PI;
+    }
+
+    get SVGPaths()
+    {
+        var result: SVGPathElement[] = [];
+        for (var index = 0; index < this.pathD.length; index++)
+        {
+            result.push(this.SVGPath(index));
+        }
+        return result;
+    }
+
+    SVGPath (index: number)
+    {
+        var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttributeNS(null, 'd', this.pathD[index]);
+        path.setAttributeNS(null, 'stroke', this.colors[index]);
+        path.setAttributeNS(null, 'fill', 'transparent');
+        return path;
+    }
+}
+
+type Vector2 = { x: number; y: number; }
